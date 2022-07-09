9816d92d6a9871a86a57d0fe7ae82795a9d4c7f8
CVB-999999999 <77160567+CVB-999999999@users.noreply.github.com>
2022-07-09 14:41:24 +0100

    Part 2

--- README.md
+++ README.md
@@ -1,17 +1,29 @@
 # Spark-2-Challenge
 
+## Setup
+
 Spark Instalation - <https://www.tutorialspoint.com/apache_spark/apache_spark_installation.htm>
 
 Spark setup - <https://sparkbyexamples.com/spark/spark-setup-run-with-scala-intellij/>
 
 Spark Create DF - <https://sparkbyexamples.com/spark/different-ways-to-create-a-spark-dataframe/>
 
-Spark DF Operation - <https://www.educba.com/spark-dataframe/>
-
 Spark DF - <https://spark.apache.org/docs/2.2.0/sql-programming-guide.html>
+
+## Part 1
+
+Spark DF Operation - <https://www.educba.com/spark-dataframe/>
 
 Change DF collumn type - <https://sparkbyexamples.com/spark/spark-change-dataframe-column-type/>
 
 Fill nan in DF - <https://stackoverflow.com/questions/43844389/spark-replace-all-nans-to-null-in-dataframe-api>
 
-Replace Collumns - https://stackoverflow.com/questions/56557771/use-map-to-replace-column-values-in-spark
+Replace Columns - <https://stackoverflow.com/questions/56557771/use-map-to-replace-column-values-in-spark>
+
+Rename Columns - <https://stackoverflow.com/questions/35592917/renaming-column-names-of-a-dataframe-in-spark-scala>
+
+## Part 2
+
+Spark write to CSV - <https://sparkbyexamples.com/spark/spark-write-dataframe-to-csv-file/>
+
+Spark Sort - <https://sparkbyexamples.com/spark/spark-sort-column-in-descending-order/>
--- Spark/src/main/scala/org/example/App.scala
+++ Spark/src/main/scala/org/example/App.scala
@@ -17,15 +17,14 @@ object App {
       .master(master).appName("app")
       .getOrCreate()
 
-    // Loads the csv
-    var gpsur = spark.read.csv("./google-play-store-apps/googleplaystore_user_reviews.csv")
-    // Test if its reading properly
-    //    df1.show()
+    /* ---- Part 1 ---- */
 
-    /***** Part 1 *****/
+    // Loads the csv
+    /*var gpsur = spark.read.csv("./google-play-store-apps/googleplaystore_user_reviews.csv")
 
     // Replace empty cell with 0
     gpsur = gpsur.na.replace("_c3", Map("nan" -> "0"))
+    gpsur = gpsur.na.replace("_c3", Map("NaN" -> "0"))
     gpsur = gpsur.na.replace("_c3", Map("null" -> "0"))
     gpsur = gpsur.na.replace("_c3", Map("" -> "0"))
 
@@ -36,11 +35,39 @@ object App {
     df1 = df1.groupBy("_c0").avg("_c3")
 
     // Change column names
-    // _c0 -> App 
+    // _c0 -> App
     // _c3 -> Average_Sentiment_Polarity
     df1 = df1.toDF("App", "Average_Sentiment_Polarity")
+*/
+    //    df1.show()
 
-    df1.show()
+    /* ---- Part 2 ---- */
+
+    // Loads the csv
+    val gps = spark.read.csv("./google-play-store-apps/googleplaystore.csv")
+
+    // Change NaN and other to 0
+    var df2 = gps.na.replace("_c2", Map("nan" -> "0"))
+    df2 = df2.na.replace("_c2", Map("NaN" -> "0"))
+    df2 = df2.na.replace("_c2", Map("null" -> "0"))
+    df2 = df2.na.replace("_c2", Map("" -> "0"))
+
+    // Change Rating column to double
+    df2 = df2.withColumn("_c2", col("_c2").cast(DoubleType)).as("_c2")
+
+    // Filter Ratings bellow 4
+    df2 = df2.filter(df2("_c2") >= 4)
+
+    // Sort by Rating Desc
+    df2 = df2.sort(col("_c2").desc)
+
+    // Save to CSV
+    // TODO: Change File name
+    df2.coalesce(1)
+      .write.format("com.databricks.spark.csv")
+      .option("header", "true")
+      .option("delimiter", "§")
+      .save("best_apps")
 
   }
 }
